1725b37d30954833488039b7369e07af19980867
Tobias Zürcher <tobias.zuercher@gmail.com>
2014-09-16 17:45:52 +0200

    bugfix with name of env

--- src/Transformer/XmlEnvironmentSerializer.cs
+++ src/Transformer/XmlEnvironmentSerializer.cs
@@ -2,21 +2,26 @@
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
-using PowerDeploy.Transformer;
+using PowerDeploy.Transformer.Logging;
+using Transformer.Logging;
 using Transformer.Model;
 
 namespace Transformer
 {
     public class XmlEnvironmentSerializer : IEnvironmentSerializer
     {
+        private static ILog _log = LogManager.GetLogger(typeof (XmlEnvironmentSerializer));
+
         public Environment Deserialize(string file)
         {
-            var xml = File.ReadAllText(file);
+            _log.DebugFormat("Deserializing environment file {0}", file);
 
+            var xml = File.ReadAllText(file);
+            
             var serializer = new XmlSerializer(typeof(Environment));
             var environment = serializer.Deserialize(new XmlTextReader(new StringReader(xml))) as Environment;
-            
-            // todo: add error handling for failed deserializing
+            environment.Name = Path.GetFileNameWithoutExtension(file);
+           
             environment.Variables.Add(new Variable() { Name = "env", Value = environment.Name.ToUpper(CultureInfo.InvariantCulture) });
 
             return environment;
