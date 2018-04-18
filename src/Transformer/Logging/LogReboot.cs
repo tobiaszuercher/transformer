1cfb9cb91638538b0f98abb139a8aafe1cdf2d63
Tobias Zürcher <tzu@Tobiass-MacBook-Pro.local>
2018-04-18 16:21:25 +0200

    colorful console

---
+++ src/Transformer/Logging/LogReboot.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using Colorful;
+using Console = Colorful.Console;
+
+namespace Transformer.Logging
+{
+    public class LogReboot
+    {
+        public void Info(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Warn(string text)
+        {
+            Console.WriteLine(text, Color.Orange);
+        }
+
+        public void Error(string text)
+        {
+            Console.WriteLine(text, Color.Red);
+        }
+
+        public void InitStylesheet(string environemnt)
+        {
+            StyleSheet styleSheet = new StyleSheet(Color.White);
+            styleSheet.AddStyle(environemnt, Color.MediumSlateBlue, match => match.ToUpper());
+
+            //Console.WriteLineStyled(storyAboutRain, styleSheet);
+        }
+    }
+}
